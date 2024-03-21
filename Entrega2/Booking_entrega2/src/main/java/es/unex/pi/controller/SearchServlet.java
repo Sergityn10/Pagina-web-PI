fc48dcab4a68a4b866459df801eb8fca19bd22d7
Sergityn10 <sermarled@gmail.com>
2024-03-21 20:24:23 +0100

    Configurando SearchServlet y el botón de buscar alojamientos de la página principal

--- Entrega2/Booking_entrega2/src/main/java/es/unex/pi/controller/SearchServlet.java
+++ Entrega2/Booking_entrega2/src/main/java/es/unex/pi/controller/SearchServlet.java
@@ -1,16 +1,23 @@
 package es.unex.pi.controller;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
+
+import es.unex.pi.dao.AccommodationDAO;
+import es.unex.pi.dao.JDBCAccommodationDAOImpl;
+import es.unex.pi.model.Accommodation;
 
 /**
  * Servlet implementation class SearchServlet
  */
-@WebServlet( urlPatterns = "/orders/SearchServlet.do")
+@WebServlet( urlPatterns = {"/orders/SearchServlet.do"})
 public class SearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,7 +34,21 @@ public class SearchServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		Connection conn = (Connection) getServletContext().getAttribute("dbConn");
+		AccommodationDAO accomDao = new JDBCAccommodationDAOImpl();
+		accomDao.setConnection(conn);
 		
+		List<Accommodation> searchList = accomDao.getAllBySearchName(request.getParameter("lugar-alojamiento"));
+		
+		if(searchList == null) {
+			request.setAttribute("messages", "No existe ningún alojamiento con los requisitos mencionados");
+			RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/index.jsp");
+			view.forward(request, response);
+		}
+		else {
+			RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/listaAlojamientos.jsp");
+			view.forward(request, response);
+		}
 	}
 
 	/**
